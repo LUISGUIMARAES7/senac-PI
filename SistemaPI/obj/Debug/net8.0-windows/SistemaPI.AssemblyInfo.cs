c75232dbab78f6a234abe4a842b21381940e26b1
LUIS FERNANDO GUIMARAES SANTOS <luis.fgsantos6@senacsp.edu.br>
2025-04-23 19:03:05 -0300

    alguns ajustes na tela inicial

--- SistemaPI/obj/Debug/net8.0-windows/SistemaPI.AssemblyInfo.cs
+++ SistemaPI/obj/Debug/net8.0-windows/SistemaPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5365206e6101eaeeb240992662184942bbe9fb08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1b17fb02c3562f821d14672178ba2216d78ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
